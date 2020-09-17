1ee32b2195875ddf246ab2084e94b6bd9ad4fa6a
Alan Richardson <arichardson@securecodewarrior.com>
2020-09-17 15:33:48 +0100

    added DisabledWithoutReasonTest to add params to @Disabled annotation

--- junitexamples/src/test/java/annotations/DisabledWithoutReasonTest.java
+++ junitexamples/src/test/java/annotations/DisabledWithoutReasonTest.java
@@ -62,12 +62,12 @@ availableFixes:
     // checkstyle sun checks finds no problems with this method
     // SonarLint flags this as an issue but SonarLint docs currently show examples for Junit4 rather than Junit 5
     @Disabled
-    void thisTestMethodHasNoDisabledComment(){
+    void thisTestMethodHasNoDisabledReason(){
         Assertions.fail("This test is disabled so should not run and we won't fail");
     }
 
     @Disabled("I have a reason")
-    void thisTestMethodHasDisabledComment(){
+    void thisTestMethodHasDisabledReason(){
         Assertions.fail("This test is disabled so should not run and we won't fail");
     }
 }
